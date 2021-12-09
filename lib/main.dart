07f205af616ba42721996239338aa901593261cf
Diego said <diego@doublelogic.com.br>
2021-12-09 16:46:15 -0300

    Final version

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,15 +11,55 @@ void main() {
           title: const Text('Dicee'),
           backgroundColor: Colors.blue,
         ),
-        body: DicePage(),
+        body: const DicePage(),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+              child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      rollDices();
+                    });
+                    print('Left button got pressed.');
+                  },
+                  child: Image.asset('images/dice$leftDiceNumber.png'))),
+          Expanded(
+              child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      rollDices();
+                    });
+                    print('Right button got pressed.');
+                  },
+                  child: Image.asset('images/dice$rightDiceNumber.png')))
+        ],
+      ),
+    );
   }
+
+  void rollDices() {
+    leftDiceNumber = _randomDiceNumber();
+    rightDiceNumber = _randomDiceNumber();
+  }
+
+  int _randomDiceNumber() => 1 + Random().nextInt(6);
 }
